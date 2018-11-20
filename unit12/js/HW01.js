d224112e861a87c9519b5e36a43ad53cfc6373da
Phan_Trong_Tuyen <phantuyen1k98@gmail.com>
2018-11-20 22:47:01 +0700

    unit12

--- unit12/js/HW01.js
+++ unit12/js/HW01.js
@@ -1,75 +1,67 @@
 $(function(){
-	var user = $('#user').val().trim();
-	var pass = $('#pass').val().trim();
-	var pass1 = $('#pass1').val().trim();
-	var first = $('#first').val();
-	var last = $('#last').val();
-	var mail = $('#mail').val();
+	
 	var address = $('#address').val();
 	var age = $('#age').val();
-	$('#user').change(function(){
-		alert(user)
-		if (user == '') {
-			$('.tb1').text('This field is required.')
-		}
-		else{
-			$('.tb1').text('')
-		}
-	})
-	$('#pass').change(function(){
-		if (pass == '') {
-			$('.tb2').text('This field is required.')
-		}
-		else{
-			$('.tb2').text('')
-		}
-	})
-	$('.pass1').change(function(){
-		if (pass1 == '') {
-			$('.tb3').text('This field is required.')
-		}
-		else{
-			$('.tb3').text('')
-		}
-	})
-	$('.first').change(function(){
-		if (first == '') {
-			$('.tb4').text('This field is required.')
-		}
-		else{
-			$('.tb4').text('')
-		}
-	})
-	$('.last').change(function(){
-		if (last == '') {
-			$('.tb5').text('This field is required.')
-		}
-		else{
-			$('.tb5').text('')
-		}
-	})
-	$('.mail').change(function(){
-		if (mail == '') {
-			$('.tb6').text('This field is required.')
-		}
-		else{
-			$('.tb6').text('')
-		}
-	})
 	$('.next1').click(function(){
+		var user = $('#user').val().trim();
+		var pass = $('#pass').val().trim();
+		var pass1 = $('#pass1').val().trim();
+		// if (user == '' || pass == ''|| pass1 == ''){
+		// 	alert('Bạn phải nhập đầy đủ các thông tin trên.')
+		// }
+		if(user == ''){
+			$('.tb1').text('Bạn không dược bỏ trống mục này.')
+		}
+		else if(pass == ''){
+			$('.tb2').text('Bạn không dược bỏ trống mục này.')
+		}
+		else if(pass1 == ''){
+			$('.tb3').text('Bạn không dược bỏ trống mục này.')
+		}
+		else if(pass1 != '' && pass1 != pass){
+			$('.tb3').text('Mật khẩu bạn điền không trùng khớp.')
+		}
+
+		else{
 			$('.ct1').css('display','none')
 			$('.ct2').css('display','block')
+		}
 	})
 	$('.next2').click(function(){
+		var first = $('#first').val();
+		var last = $('#last').val();
+		var mail = $('#mail').val();
+		// if(first == ''|| last ==''||mail==''){
+		// 	alert('Bạn phải điền đầy đủ những thông tin cần thiết (có dấu *)')
+		// }
+		if(first == ''){
+			$('.tb4').text('Bạn không được bỏ trống mục này.')
+		}
+		else if(last == ''){
+			$('.tb5').text('Bạn không được bỏ trống mục này.')
+		}
+		else if(!IsEmail(mail)&& mail != '') {
+			$('.tb6').text('Vui lòng nhập đúng định dạng email')
+		}
+		else if(mail == ''){
+			$('.tb6').text('Bạn không được bỏ trống mục này.')
+		}
+		else{
 			$('.ct2').css('display','none')
 			$('.ct3').css('display','block')
+		}
 	})
 	$('.next3').click(function(){
 			$('.ct3').css('display','none')
 			$('.ct4').css('display','block')
 	})
-	$('.finish').click(function(){	
-			alert('Submit.')
+	$('.finish').click(function(){
+		if($('#checkbox').is(':checked')==true){	
+			alert('Submited.')
+		}
+		else{
+			alert('Bạn phải chấp nhận điều khoản.')
+		}
 	})
 
 	$('.prev1').click(function(){
@@ -86,5 +78,13 @@ $(function(){
 			$('.ct4').css('display','none')
 			$('.ct3').css('display','block')
 	})
+	function IsEmail(email) {
+		var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(!regex.test(email)) {
+		  return false;
+		}else{
+		    return true;
+		}
+	}
 
 })
